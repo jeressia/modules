798682c3bd7aaa09078a92fab738615123231abe
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-08 21:25:52 -0500

    imported somethings

---
+++ javascripts/components/store.js
@@ -0,0 +1,8 @@
+import util from '../helpers/util.js'
+
+const makeStore = () => {
+    let domString = '<h4>Jeressia Jay</h4>';
+    util.printToDom('store-container',domString);
+};
+
+export default {makeStore};
