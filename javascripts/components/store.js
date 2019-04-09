05722c93acd97aa0c7d282da8d59cbda502b8c19
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-08 21:34:15 -0500

    added book image

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,7 +1,12 @@
-import util from '../helpers/util.js'
+import book from '../helpers/book.js'
+import util from '../helpers/util.js';
 
 const makeStore = () => {
-    let domString = '<h4>Jeressia Jay</h4>';
+    const bookInfo = book.getBook();
+    let domString = `<h2> Our Only Book</h2>`;
+    domString += `<h3>${bookInfo.price}</h3>`
+    domString += `<img class ="bookImg" src=${bookInfo.image} alt="book cover">`
+    domString += `<button class="btn btn-danger">Add to Cart</button>`
     util.printToDom('store-container',domString);
 };
 
